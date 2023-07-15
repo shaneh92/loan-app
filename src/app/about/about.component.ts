3dd58b8a91851b7acd48a80d55ac3edae58e20a6
Shane Hingtgen <shanehingtgen@gmail.com>
2023-07-14 22:00:34 -0500

    backing up

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,27 @@
+/*
+    ========================================================================================================================
+    ; Title: about.component.ts
+    ; Author: Shane Hingtgen
+    ; Bellevue University
+    ; Date: 7/14/23
+    ; Description: Capstone
+    ; Work Cited:
+    Coding Guidelines
+    Instructions - Assignment 6.4 - Input Properties
+    Instruction - Exercise 7.3 Form Validation
+    Course GitHub
+    ========================================================================================================================
+ */
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
